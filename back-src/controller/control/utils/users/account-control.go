29a900c1312402bf51e76c5708e4811ef31989e4
Ashk <ashkan.khd.q@gmail.com>
2020-08-30 00:47:21 +0430

    merge ashk and sol

--- back-src/controller/control/utils/users/account-control.go
+++ back-src/controller/control/utils/users/account-control.go
@@ -16,32 +16,6 @@ func RegisterEmployer(emp existence.Employer, Db *database.Database) error {
 	return errors.New("duplicate username: " + emp.Username)
 }
 
-func EditEmployerProfile(emp existence.Employer, DB *database.Database) error {
-	if !DB.DoesEmployerExistWithUsername(emp.Username) {
-		return errors.New("no user with such username :" + emp.Username)
-	}
-
-	if err := DB.UpdateEmployer(emp.Username, emp); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func GetEmployerProfile(username string, DB *database.Database) (existence.Employer, error) {
-	if !DB.DoesEmployerExistWithUsername(username) {
-		return existence.Employer{}, errors.New("no user with such username :" + username)
-	}
-	return DB.GetEmployer(username)
-}
-
-func GetEmployerProjects(username string, DB *database.Database) ([]existence.Project, error) {
-	if !DB.DoesEmployerExistWithUsername(username) {
-		return nil, errors.New("no user with such username :" + username)
-	}
-	return DB.GetEmployerProjects(username)
-}
-
 func RegisterFreelancer(frl existence.Freelancer, Db *database.Database) error {
 	if !Db.DoesFreelancerExistWithUsername(frl.Username) {
 		if !Db.DoesEmployerExistWithEmail(frl.Email) {
