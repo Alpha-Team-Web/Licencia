1c9590b4d568fa629916bf4f29f11eafa0aa6e94
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2020-08-30 18:46:54 +0430

    Starting Connecting To Sever

--- front-src/Js Functionals/ProfilePage/JS1.js
+++ front-src/Js Functionals/ProfilePage/JS1.js
@@ -6,10 +6,21 @@ const siteAddressField= document.getElementById("siteAddressField");
 const telephoneNumberField= document.getElementById("telephoneNumberField");
 const passwordField= document.getElementById("passwordField");
 const repeatPasswordField= document.getElementById("repeatPasswordField");
+const gitHubAccountField = document.getElementById("githubAccountField");
 const descriptionField= document.getElementById("descriptionField");
 const addressField= document.getElementById("addressField");
-
 function logOut() {
     // Todo
     window.location.href = mainPageName;
+}
+
+const gitHubAccountPart = document.getElementById("gitHubAccountPart");
+function loadProfileMenu() {
+    if (false /* if is Not FreeLancer */) {
+        gitHubAccountPart.style.display = "none";
+    }
+    console.log(Cookies.get('Fuck', {
+        domain: "FuckFuckFuck",
+        path: "FuckFuck"
+    }));
 }
