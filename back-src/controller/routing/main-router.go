5ba91815634f5192d4fe425bdf7c03f66d8d5e57
soligits <m.soltani.252@gmail.com>
2020-08-31 18:57:26 +0430

    Small adjustments.

--- back-src/controller/routing/main-router.go
+++ back-src/controller/routing/main-router.go
@@ -12,9 +12,9 @@ type Listener interface {
 }
 
 type router struct {
-	port       string
-	server     *gin.Engine
-	controller *handle.Handler
+	port    string
+	server  *gin.Engine
+	handler *handle.Handler
 }
 
 func NewRouter(port string) Listener {
@@ -25,28 +25,28 @@ func NewRouter(port string) Listener {
 
 func (router *router) Listen() error {
 	router.server.POST("/register", func(context *gin.Context) {
-		view.RespondRegister(context, router.controller.Register(context))
+		view.RespondRegister(context, router.handler.Register(context))
 	})
 
 	router.server.POST("/login", func(context *gin.Context) {
-		token, err := router.controller.Login(context)
+		token, err := router.handler.Login(context)
 		if err == nil {
-			router.controller.AddNewClock(token)
+			router.handler.AddNewClock(token)
 		}
 		view.RespondLogin(context, token, err)
 	})
 
 	router.server.POST("/employer/edit-profile", func(context *gin.Context) {
-		users.RespondEmployerEditProfile(context, router.controller.EditEmployerProfile(context))
+		users.RespondEmployerEditProfile(context, router.handler.EditEmployerProfile(context))
 	})
 
 	router.server.GET("/employer/get-profile", func(context *gin.Context) {
-		emp, token, err := router.controller.GetEmployerProfile(context)
+		emp, token, err := router.handler.GetEmployerProfile(context)
 		users.RespondEmployerGetProfile(context, token, emp, err)
 	})
 
 	router.server.GET("/freelancer/get-profile", func(context *gin.Context) {
-		emp, token, err := router.controller.GetFreelancerProfile(context)
+		emp, token, err := router.handler.GetFreelancerProfile(context)
 		users.RespondFreelancerGetProfile(context, token, emp, err)
 	})
 
--- back-src/model/existence/users.go
+++ back-src/model/existence/users.go
@@ -9,8 +9,8 @@ type user struct {
 	Username    string `json:"username" binding:"min=4,max=20" sql:"username,pk,notnull"`
 	Password    string `json:"password" binding:"min=6,max=20" sql:"password,notnull"`
 	ShownName   string `json:"shown-name" binding:"min=4,max=30" sql:",notnull"`
-	Email       string `json:"email" binding:"email,min=5,max=100" pg:"email,unique"`
-	Description string `json:"description" pg:"description"`
+	Email       string `json:"email" binding:"email,min=5,max=100" sql:"email,unique"`
+	Description string `json:"description" sql:"description"`
 }
 
 type person struct {
