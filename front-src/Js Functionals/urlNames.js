6d6510121501401dc42915b81de6d42e991c91d8
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2020-09-01 03:17:08 +0430

    ProfilePage Half

--- front-src/Js Functionals/urlNames.js
+++ front-src/Js Functionals/urlNames.js
@@ -1,9 +1,12 @@
-const urlSignUp = "http://localhost:8008/register"
-const urlLogin = "http://localhost:8008/login"
-const urlGetEmployerProfileInfo = "http://localhost:8008/employer/get-profile"
-const urlGetFreelancerProfileInfo = "http://localhost:8008/freelancer/get-profile"
+const urlSignUp = "http://localhost:8008/io/register"
+const urlLogin = "http://localhost:8008/io/login"
+const urlGetEmployerProfileInfo = "http://localhost:8008/employer/profile/get"
+const urlGetFreelancerProfileInfo = "http://localhost:8008/freelancer/profile/get"
 const gitHubUrl = "https://github.com/"
 
-const saveProfileUrl="";
-const saveGithubUrl="";
-const changePasswordUrl="";
+const saveProfileUrlEmployer="http://localhost:8008/employer/profile/general";
+const saveGithubUrlEmployer="http://localhost:8008/employer/profile/links";
+const changePasswordUrlEmployer="http://localhost:8008/employer/profile/password";
+const saveProfileUrlFreeLancer="http://localhost:8008/freelancer/profile/general";
+const saveGithubUrlFreeLancer="http://localhost:8008/freelancer/profile/links";
+const changePasswordUrlFreeLancer="http://localhost:8008/freelancer/profile/password";
