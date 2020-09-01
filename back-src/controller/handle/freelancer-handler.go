ba4ca4b6cbcb0c6b9763324b0634df416bb2e0b3
soligits <m.soltani.252@gmail.com>
2020-09-01 22:05:57 +0430

    Fix handler token bug.

--- back-src/controller/handle/freelancer-handler.go
+++ back-src/controller/handle/freelancer-handler.go
@@ -8,47 +8,43 @@ import (
 )
 
 func (handler *Handler) EditFreelancerProfile(ctx *gin.Context) (string, error) {
-	token := ctx.GetHeader("Token")
-	if newToken, err := CheckToken(token, existence.EmployerType); err != nil {
+	if newToken, err := CheckToken(ctx.GetHeader("Token"), existence.EmployerType); err != nil {
 		return "", err
 	} else {
 		frl := existence.Freelancer{}
 		if err := ctx.ShouldBindJSON(&frl); err != nil {
 			return newToken, err
 		}
-		return newToken, users.EditFreelancerProfile(token, frl, DB)
+		return newToken, users.EditFreelancerProfile(newToken, frl, DB)
 	}
 }
 
 func (handler *Handler) EditFreelancerPassword(ctx *gin.Context) (string, error) {
-	token := ctx.GetHeader("Token")
-	if newToken, err := CheckToken(token, existence.EmployerType); err != nil {
+	if newToken, err := CheckToken(ctx.GetHeader("Token"), existence.EmployerType); err != nil {
 		return "", err
 	} else {
 		frl := data.ChangePassRequest{}
 		if err := ctx.ShouldBindJSON(&frl); err != nil {
 			return newToken, err
 		}
-		return newToken, users.EditFreelancerPassword(token, frl, DB)
+		return newToken, users.EditFreelancerPassword(newToken, frl, DB)
 	}
 }
 
 func (handler *Handler) EditFreelancerLinks(ctx *gin.Context) (string, error) {
-	token := ctx.GetHeader("Token")
-	if newToken, err := CheckToken(token, existence.EmployerType); err != nil {
+	if newToken, err := CheckToken(ctx.GetHeader("Token"), existence.EmployerType); err != nil {
 		return "", err
 	} else {
 		frl := existence.Freelancer{}
 		if err := ctx.ShouldBindJSON(&frl); err != nil {
 			return newToken, err
 		}
-		return newToken, users.EditFreelancerLinks(token, frl, DB)
+		return newToken, users.EditFreelancerLinks(newToken, frl, DB)
 	}
 }
 
 func (handler *Handler) GetFreelancerProfile(ctx *gin.Context) (existence.Freelancer, string, error) {
-	token := ctx.GetHeader("Token")
-	if newToken, err := CheckToken(token, existence.FreelancerType); err != nil {
+	if newToken, err := CheckToken(ctx.GetHeader("Token"), existence.FreelancerType); err != nil {
 		return existence.Freelancer{}, "", err
 	} else {
 		frl, err := users.GetFreelancer(newToken, DB)
