3743decf04c326de6f2d0b638da447df53d3853d
Ashk <ashkan.khd.q@gmail.com>
2020-09-03 03:12:07 +0430

    testing filters

--- back-src/controller/routing/project-router.go
+++ back-src/controller/routing/project-router.go
@@ -7,9 +7,6 @@ func (router *router) addProjectEndpoints() {
 	router.addNewEndpointGroup("/filter", "filter", "project")
 
 	router.addHandlerToPath("/freelancer", "filter", Get, func(ctx *gin.Context) {
-		//handle
-
-		//respond
 
 	})
 
