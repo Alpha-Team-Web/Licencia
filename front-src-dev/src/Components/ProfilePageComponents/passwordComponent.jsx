a9b05b1a43061e68239583d5e631f132d77e5755
amirsadra79 <58373783+amirsadra79@users.noreply.github.com>
2020-09-07 15:37:24 +0430

    for pull

--- front-src-dev/src/Components/ProfilePageComponents/passwordComponent.jsx
+++ front-src-dev/src/Components/ProfilePageComponents/passwordComponent.jsx
@@ -1,13 +1,36 @@
-import React, {Component} from 'react';
+import React from 'react'
+import { Button, Header, Image, Modal } from 'semantic-ui-react'
+import {greenColor, loginMenu, signUpMenu} from "../../Js Functionals/MainPage/Login SignUp Show";
+import licenciaImg from "../../Pics/Licencia-Logo.png";
+import MainLoginMenu from "./mainLoginMenu";
+import MainSignUpMenu from "./mainSignUpMenu";
 
-class PasswordComponent extends Component {
-    render() {
-        return (
-            <div>
+function ModalLogSin() {
+    const [open, setOpen] = React.useState(false)
 
-            </div>
-        );
-    }
+    return (
+        <Modal
+            onClose={() => setOpen(false)}
+            onOpen={() => setOpen(true)}
+            open={open}
+            trigger={<Button className="loginButton">ورود / ثبت نام</Button>}
+        >
+            <Modal.Content>
+                <div className="header" id="Login-Menu-Header">
+                    <div id="Signup-Login">
+                        <div style={style2} className="Signup-login-text" id="LoginMenuButton" onClick={() => loginMenu()}>ورود</div>
+                        <div className="Signup-login-text" id="SignUpMenuButton" onClick={() => signUpMenu()}>ثبت نام</div>
+                    </div>
+                    <div className="image content">
+                        <img src={licenciaImg} id="logoImage" alt="logoLicencia"/>
+                    </div>
+                    <h3 id="welcomeHeader">Welcome To Licencia</h3>
+                </div>
+                <MainLoginMenu id='Login-Menu'/>
+                <MainSignUpMenu style = {style1} id='SignUp-Menu'/>
+            </Modal.Content>
+        </Modal>
+    )
 }
 
-export default PasswordComponent;
+export default ModalLogSin
