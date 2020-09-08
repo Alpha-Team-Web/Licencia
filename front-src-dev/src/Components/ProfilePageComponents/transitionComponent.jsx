55daa56e45fe033ee7e3c24c57b0383727e9b115
amirsadra79 <58373783+amirsadra79@users.noreply.github.com>
2020-09-08 22:47:58 +0430

    push

--- front-src-dev/src/Components/ProfilePageComponents/transitionComponent.jsx
+++ front-src-dev/src/Components/ProfilePageComponents/transitionComponent.jsx
@@ -1,18 +1,37 @@
-import React, { Component } from 'react'
-import { Button, Divider, Image, Transition } from 'semantic-ui-react'
+import React, {Component} from 'react'
+import {Button, Divider, Image, Transition} from 'semantic-ui-react'
 
 export default class TransitionComponent extends Component {
-    state = {
-        visible: this.props.visibility
-    };
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            visible: props.visibility
+        };
+        this.changeState = this.changeState.bind(this);
+    }
+
+    state;
+
+    changeState() {
+        alert("visible before : " + this.state.visible)
+        this.setState((prevState) => {
+            {
+                alert('visible alan:' + prevState.visible)
+                return {visible: !prevState.visible};
+                
+            }
+        })
+        alert("visible now : " + this.state.visible)
+    }
 
     render() {
         alert('visibility: ' + this.state.visible)
 
         return (
-                <Transition id={this.props.id} visible={this.state.visible} animation={this.props.animation} duration={this.props.duration == undefined ? 500 : this.props.duration}>
-                    {this.props.content}
-                </Transition>
+            <Transition  visible={this.state.visible} animation={this.props.animation}
+                        duration={500}>
+                {this.props.content}
+            </Transition>
         )
     }
 }
