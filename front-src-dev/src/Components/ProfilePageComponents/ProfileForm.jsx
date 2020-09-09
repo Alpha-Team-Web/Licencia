c2cc1f5b62dc9a0c28cad29fd7fd224c8ed9f5ed
solale427 <seiedesolale@gmail.com>
2020-09-09 14:01:09 +0430

    right align

--- front-src-dev/src/Components/ProfilePageComponents/ProfileForm.jsx
+++ front-src-dev/src/Components/ProfilePageComponents/ProfileForm.jsx
@@ -10,57 +10,58 @@ class ProfileForm extends Component {
     render() {
         return (
             <Segment style={this.props.style} id={this.props.id}>
-                <Grid className="flexRow" relaxed='very'>
+                <Grid className="flexRow ui-rtl" relaxed='very'>
+                    <Grid.Column width={8} >
+                        <div className="ui form ui-rtl">
+                            <div className="two fields ">
+                                <div className="six wide field ui-rtl">
+                                    <label className='rightAligned no-space-break label-size'> نام کاربری</label>
+                                    <input className='rightAligned' type="text" placeholder="Username" id="usernameField"></input>
+                                </div>
+                                <div className="six wide field ui-rtl">
+                                    <label className='rightAligned no-space-break label-size'>نام نمایشی</label>
+                                    <input className="" type="text" placeholder="Showing Name" id="showingNameField"></input>
+                                </div>
+                            </div>
+                            <div className="two fields ui-rtl">
+                                <div className="six wide field ui-rtl">
+                                    <label>نام</label>
+                                    <input  type="text" placeholder="First Name" id="firstNameField"></input>
+                                </div>
+                                <div className="six wide field ui-rtl">
+                                    <label>نام خانوادگی</label>
+                                    <input  type="text" placeholder="Last Name" id="lastNameField"></input>
+                                </div>
+                            </div>
+                            <div className="twenty wide field ui-rtl">
+                                <label>E-mail</label>
+                                <input className="input-size" type="email" placeholder="Email" id="emailField"></input>
+                            </div>
+                            <div className="two fields ui-rtl">
+                                <div className="six wide field">
+                                    <label>آدرس</label>
+                                    <input className="input-size" type="text" placeholder="Address" id="addressField"></input>
+                                </div>
+                                <div className="six wide field">
+                                    <label className='rightAligned no-space-break label-size'>شماره تلفن</label>
+                                    <input  className="input-size" type="text" placeholder="Phone Number" id="telephoneNumberField"></input>
+                                </div>
+                            </div>
+                            <div className=" ui-rtl">
+                                <label>توضیحات</label>
+                                <textarea rows="3" placeholder="Description" id="descriptionField"></textarea>
+                            </div>
+                            <button className="ui button" >
+                                confirm
+                            </button>
+                        </div>
+                    </Grid.Column>
                     <Grid.Column width={5}>
                         <div className="flexColumn" id="leftDiv">
                             <div className="content" id="ImageTippoContent">
                                 <img className="ui circular bordered image" src={profilePic}
                                      alt="*User*'s Picture"/>
                             </div>
-                        </div>
-                    </Grid.Column>
-                    <Grid.Column width={8} >
-                        <div className="ui form ">
-                            <div className="two inline fields ">
-                                <div className="six wide field">
-                                    <input className='rightAligned input-size' type="text" placeholder="Username" id="usernameField"></input>
-                                    <label className='rightAligned no-space-break label-size'> نام کاربری</label>
-                                </div>
-                                <div className="six wide field">
-                                    <input className="input-size" type="text" placeholder="Showing Name" id="showingNameField"></input>
-                                    <label className='rightAligned no-space-break label-size'>نام نمایشی</label>
-                                </div>
-                            </div>
-                            <div className="two inline fields">
-                                <div className="six wide field">
-                                    <input className="input-size" type="text" placeholder="Last Name" id="lastNameField"></input>
-                                </div>
-                                <div className="six wide field">
-                                    <input className="input-size" type="text" placeholder="First Name" id="firstNameField"></input>
-                                    <label>نام</label>
-                                </div>
-                            </div>
-                            <div className="twenty wide field inline">
-                                <input className="input-size" type="email" placeholder="Email" id="emailField"></input>
-                                <label>E-mail</label>
-                            </div>
-                            <div className="two inline fields">
-                                <div className="six wide field">
-                                    <input className="input-size" type="text" placeholder="Address" id="addressField"></input>
-                                    <label>آدرس</label>
-                                </div>
-                                <div className="six wide field">
-                                    <input  className="input-size" type="text" placeholder="Phone Number" id="telephoneNumberField"></input>
-                                    <label className='rightAligned no-space-break label-size'>شماره تلفن</label>
-                                </div>
-                            </div>
-                            <div className="fields inline">
-                                <textarea rows="3" placeholder="Description" id="descriptionField"></textarea>
-                                <label>توضیحات</label>
-                            </div>
-                            <button className="ui button" >
-                                confirm
-                            </button>
                         </div>
                     </Grid.Column>
                 </Grid>
--- front-src-dev/src/Components/ProfilePageComponents/profileHeader.jsx
+++ front-src-dev/src/Components/ProfilePageComponents/profileHeader.jsx
@@ -2,29 +2,34 @@ import React, { Component } from 'react';
 import '../../CSS Designs/Header.css';
 import '../../CSS Designs/basic.css';
 import '../../CSS Designs/extra-css.css'
+import GridColumn, {Grid} from "semantic-ui-react";
 
 class Menu extends Component {
     state = {  }
     render() {
         return (
-            <div className=" header">
-                <div className="ui teal four borderless menu">
-                    <div className="item">
-                        <button className="ui labeled icon negative basic huge button rightAligned">
-                            <i class="arrow alternate circle left icon"></i>
-                            خروج
-                        </button>
-                    </div>
-                    <div className="ui borderless menu menu-item">
-                        <a className="item ">دنبال کننده ها</a>
-                        <a className="item">دنبال شونده ها</a>
-                        <a className="item">مشاهده</a>
-                        <a className="item">دسته بندی ها</a>
-                    </div>
+        <div className="ui grid ui-rtl">
+            <div className="right floated five wide column">
+                <div className="ui borderless menu menu-item">
+                    <a className="item ">دنبال کننده ها</a>
+                    <a className="item">دنبال شونده ها</a>
+                    <a className="item">مشاهده</a>
+                    <a className="item">دسته بندی ها</a>
                 </div>
             </div>
+            <div className="left floated five wide column">
+                <div className="item">
+                    <button className="ui labeled icon negative basic huge button rightAligned">
+                        <i className="arrow alternate circle left icon"></i>
+                        خروج
+                    </button>
+                </div>
+            </div>
+
+        </div>
+
         );
     }
-}1
+}
 
 export default Menu;
