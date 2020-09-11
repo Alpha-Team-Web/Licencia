951743586d21fd53ba2f6b9aacf71f7c036ae4a1
amirsadra79 <58373783+amirsadra79@users.noreply.github.com>
2020-09-12 02:17:34 +0430

    finitto

--- front-src-dev/src/Components/MainPageComponents/mainLoginMenu.jsx
+++ front-src-dev/src/Components/MainPageComponents/mainLoginMenu.jsx
@@ -7,29 +7,41 @@ import {login} from "../../Js Functionals/MainPage/IOMethods/loginMethods";
 import '../../CSS Designs/extra-css.css'
 import {emailMaxLengthInput, passwordMaxLengthInput} from "../../Js Functionals/MainPage/ioInputLengths";
 import MainSelect from "./mainFormElements/mainSelect";
+import MainInput2 from "./mainFormElements/mainInput2";
 
 class MainLoginMenu extends Component {
     render() {
+        let options = [{
+            value: 'employer',
+            child: 'کارفرما'
+        }, {
+            value: 'freelancer',
+            child: 'فریلنسر'
+        }]
+
         return (
-            <div id={this.props.id} style={this.props.style} className="content Login-SignUp-Menu" >
+            <div id={this.props.id} style={this.props.style} className="content Login-SignUp-Menu">
                 <div className="ui form formPadding ui-rtl">
 
-                    <MainInput id='login-KeyPoint' maxLength={emailMaxLengthInput} textName='نام کاربری یا ایمیل' placeHolder='Username Or Email' errorId="loginKeyPointError"/>
+                    <MainInput2 id='login-KeyPoint' maxLength={emailMaxLengthInput} textName='نام کاربری یا ایمیل'
+                               placeHolder='Username Or Email' errorId="loginKeyPointError"/>
 
-                    <MainInput id='login-Password' maxLength={passwordMaxLengthInput} textName='رمز عبور' placeHolder='Password' isPassword={true} errorID="loginPasswordError"/>
+                    <MainInput2 id='login-Password' maxLength={passwordMaxLengthInput} textName='رمز عبور'
+                               placeHolder='Password' isPassword={true} errorID="loginPasswordError"/>
 
-                    <MainSelect id='loginKind' value1 = 'employer' textValue1='کارفرما' value2 = 'freelancer' textValue2 = 'فریلنسر' textName='نوع'/>
+                    <MainSelect id='loginKind' options={options} textName='نوع'/>
 
                     <div className="ui form formPadding" id="Login-Footer-Form">
                         <label>
                             <input onClick={() => {
                                 login(this.props.onClose)
                             }} type="submit" id="loginButton" className="ui green button"
-                                   value="login" />
+                                   value="login"/>
                         </label>
                         <footer className="loginFooter">
                             <p className="div-div-footer-p" id="LoginMenu-SignUp-Link">
-                                <Label as='a' onClick={() => signUpMenu()} className="div-div-footer-signupLink" >ثبت نام</Label>
+                                <Label as='a' onClick={() => signUpMenu()} className="div-div-footer-signupLink">ثبت
+                                    نام</Label>
                                 آیا حساب ندارید؟
                             </p>
                         </footer>
