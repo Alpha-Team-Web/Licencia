1e4e09b72200d29bdd2fe7fe8fca22c72e1103b6
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2020-09-13 00:12:02 +0430

    Ye Refactor

---
+++ front-src-dev/src/Components/SkillPageComponents/fieldCardComponent.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class FieldCardComponent extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default FieldCardComponent;
