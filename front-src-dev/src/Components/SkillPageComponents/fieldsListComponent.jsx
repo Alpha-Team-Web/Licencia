6602a88de484a05b0a09cf8f3d43c4a39fad0f6c
amirsadra79 <58373783+amirsadra79@users.noreply.github.com>
2020-09-13 21:26:15 +0430

    fieldsList component

---
+++ front-src-dev/src/Components/SkillPageComponents/fieldsListComponent.jsx
@@ -0,0 +1,77 @@
+import React, {Component} from 'react';
+import Grid from "semantic-ui-react/dist/commonjs/collections/Grid";
+import {GridColumn, GridRow} from "semantic-ui-react";
+import ReactDOM from 'react-dom';
+import FieldCardComponent from "./fieldCardComponent";
+
+class FieldsListComponent extends Component {
+
+
+    constructor(props, context) {
+        super(props, context);
+        this.setState({
+            optionsObject:this.props.skillCardObject,
+            optionsCard:this.props.skillCardObject.map((object)=>this.createCard(object))
+        })
+    }
+
+    state={
+        optionsCard:[],
+        optionsObject:[],
+
+
+    };
+
+    render() {
+        return (
+            this.createGrid()
+        );
+    }
+
+    createGrid = () => {
+        let grid = <Grid columns={this.props.columnSize} divided/>
+        let skillCardArray = this.state.skillCardArray;
+        let numberOfRows = (skillCardArray.length) / this.props.columnSize;
+        if (skillCardArray.length % this.props.columnSize !== 0) {
+            numberOfRows += 1;
+        }
+        let listOfRows = [];
+        let counter = 0
+        for (let i = 0; i < numberOfRows - 1; i++) {
+            let gridRow = <Grid.Row/>
+            let listOfColumns = []
+            for (let j = 0; j < this.props.columnSize; j++) {
+                let gridColumn = <GridColumn/>
+                let card = this.createCard(skillCardArray[counter])
+                ReactDOM.render(card, gridColumn);
+                counter += 1;
+                listOfColumns[listOfColumns.length] = gridColumn;
+            }
+            ReactDOM.render(listOfColumns, gridRow);
+            listOfRows[listOfRows.length] = gridRow;
+        }
+        let gridRow = <GridRow/>
+        let listOfColumns = []
+        for (let i = counter; i < skillCardArray.length; i++) {
+            let gridColumn = <GridColumn/>
+            ReactDOM.render(skillCardArray[i], gridColumn);
+            listOfColumns[listOfColumns.length] = gridColumn;
+        }
+        ReactDOM.render(listOfColumns, gridRow);
+        listOfRows[listOfRows.length] = gridRow;
+        ReactDOM.render(listOfRows, grid);
+        return grid;
+    }
+
+    createCard=(object)=>{
+        let card = <FieldCardComponent onClick={()=>this.showSkills(object)} />
+        return card;
+    }
+
+    showSkills=(object)=>{
+
+
+    }
+}
+
+export default FieldsListComponent;
