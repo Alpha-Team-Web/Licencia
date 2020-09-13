7a23f12336f4c5d08c6c94c4f019dca013797db2
solale427 <seiedesolale@gmail.com>
2020-09-13 16:57:25 +0430

    skillComponent.jsx and header

--- front-src-dev/src/Components/SkillPageComponents/fieldCardComponent.jsx
+++ front-src-dev/src/Components/SkillPageComponents/fieldCardComponent.jsx
@@ -4,7 +4,15 @@ class FieldCardComponent extends Component {
     render() {
         return (
             <div>
-
+                <Card>
+                    <i>
+                    </i>
+                    <Card.Content>
+                        <Card.Header>
+                            {this.props.skillName}
+                        </Card.Header>
+                    </Card.Content>
+                </Card>
             </div>
         );
     }
