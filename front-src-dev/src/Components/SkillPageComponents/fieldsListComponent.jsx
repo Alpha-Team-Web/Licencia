ad50fab93c27440069442c79fdf36d33facd7135
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2020-09-13 16:54:01 +0430

    In The Middle Of The Road

---
+++ front-src-dev/src/Components/SkillPageComponents/fieldsListComponent.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+class FieldsListComponent extends Component {
+
+
+    constructor(props, context) {
+        super(props, context);
+    }
+
+
+    state={
+        options: this.props.cards,
+    }
+
+    // transitioner = (card) =>
+
+
+    render() {
+
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default FieldsListComponent;
